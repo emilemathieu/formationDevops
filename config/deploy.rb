08e6b359f04d0cf87fe43ccd4e1527e4ff5da4e5
mathieue <emile.mathieu@eleves.enpc.fr>
2015-11-25 11:23:04 +0100

    change config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'formationDevops'
+set :repo_url, 'git@github.com:emilemathieu/formationDevops.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -33,6 +33,11 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+# ssh config
+set :ssh_options, {
+ forward_agent: true,
+}
 
 namespace :deploy do
 
