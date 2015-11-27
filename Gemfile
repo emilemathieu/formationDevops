6e6edc2c5ccfcab8e0fadb40e2c94e6babe026dd
Emile Mathieu <emilem@bamlab.fr>
2015-11-27 18:08:31 +0100

    improve config

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source "https://rubygems.org"
 
 gem 'capistrano', '~>3.4.0'
-gem 'capistrano-symfony', '~> 0.1', :github => 'capistrano/symfony'
-gem 'capistrano-composer'
+gem 'capistrano-symfony', '~> 0.2', :github => 'capistrano/symfony'
--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,66 @@ set :use_sudo, true
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+####################capistrano-symfony################
+# Symfony environment
+#set :symfony_env,  "prod"
+
+# Symfony application path
+#set :app_path,              "app"
+
+# Symfony web path
+#set :web_path,              "web"
+
+# Symfony log path
+#set :log_path,              fetch(:app_path) + "/logs"
+
+# Symfony cache path
+#set :cache_path,            fetch(:app_path) + "/cache"
+
+# Symfony config file path
+#set :app_config_path,       fetch(:app_path) + "/config"
+
+# Controllers to clear
+#set :controllers_to_clear, ["app_*.php"]
+
+# Files that need to remain the same between deploys
+#set :linked_files,          []
+
+# Dirs that need to remain the same between deploys (shared dirs)
+#set :linked_dirs,           [fetch(:log_path), fetch(:web_path) + "/uploads"]
+
+# Dirs that need to be writable by the HTTP Server (i.e. cache, log dirs)
+set :file_permissions_paths,         [fetch(:log_path), fetch(:cache_path)]
+
+# Name used by the Web Server (i.e. www-data for Apache)
+set :file_permissions_users, ['www-data']
+
+# Name used by the Web Server (i.e. www-data for Apache)
+set :webserver_user,        "www-data"
+
+# Method used to set permissions (:chmod, :acl, or :chgrp)
+set :permission_method,     :acl
+
+# Execute set permissions
+set :use_set_permissions,   true
+
+# Symfony console path
+#set :symfony_console_path, fetch(:app_path) + "/console"
+
+# Symfony console flags
+#set :symfony_console_flags, "--no-debug"
+
+# Assets install path
+#set :assets_install_path,   fetch(:web_path)
+
+# Assets install flags
+#set :assets_install_flags,  '--symlink'
+
+# Assetic dump flags
+#set :assetic_dump_flags,  ''
+
+#fetch(:default_env).merge!(symfony_env: fetch(:symfony_env))
+
 # ssh config
 set :ssh_options, {
  forward_agent: true,
